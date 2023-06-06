40f9339637453d9cd9eef6e1ba7e3a7a55c433e8
SanketWashimkar <133958334+SanketWashimkar@users.noreply.github.com>
2023-06-06 14:48:52 +0530

    main:3rd commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import { Button } from 'primereact/button';
+import { Calendar } from 'primereact/calendar';
 import './App.css';
 
 function App() {
+  const [date, setDate] = useState<any>(null);
   return (
-  <div>
+    <div>
+    <div className="card flex justify-content-center">
+    <Button label="Check" icon="pi pi-check" />
+    <br></br>
+</div>
+   <div className="card flex justify-content-center">
+   <Calendar value={date} onChange={(e) => setDate(e.value)} showIcon />
+</div>
 
-  </div>
+</div>
   );
 }
 
