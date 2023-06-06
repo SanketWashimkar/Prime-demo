1fda891db513148f1016acd2b95847952f6a51f1
SanketWashimkar <133958334+SanketWashimkar@users.noreply.github.com>
2023-06-06 15:00:53 +0530

    main:4th commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 import { Button } from 'primereact/button';
 import { Calendar } from 'primereact/calendar';
+import { InputSwitch, InputSwitchChangeEvent } from "primereact/inputswitch";
 import './App.css';
 
 function App() {
   const [date, setDate] = useState<any>(null);
+  const [checked, setChecked] = useState<any>(false);
   return (
     <div>
     <div className="card flex justify-content-center">
@@ -14,6 +16,11 @@ function App() {
    <div className="card flex justify-content-center">
    <Calendar value={date} onChange={(e) => setDate(e.value)} showIcon />
 </div>
+<br></br>
+<div className="card flex justify-content-center">
+            <InputSwitch checked={checked} onChange={(e: InputSwitchChangeEvent) => setChecked(e.value)} />
+        </div>
+
 
 </div>
   );
