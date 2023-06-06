f0723f2be2ce93e978dfccb354f8b9763257b860
SanketWashimkar <133958334+SanketWashimkar@users.noreply.github.com>
2023-06-06 14:40:00 +0530

    main:2nd commit

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,19 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+//theme
+import "primereact/resources/themes/lara-light-indigo/theme.css";     
+    
+//core
+import "primereact/resources/primereact.min.css";                                       
+        
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+ 
     <App />
-  </React.StrictMode>
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
